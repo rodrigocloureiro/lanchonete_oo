48966fa91ffe48968ac4525164334b0cd2353d30
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-08-27 17:20:29 -0300

    get & set prato.cs

---
+++ Prato.cs
@@ -0,0 +1,27 @@
+﻿namespace lanchonete_OO
+{
+    public class Prato
+    {
+        private double _precoVenda;
+        private string _dataValidade;
+        private double _peso;
+
+        public double PrecoVenda
+        {
+            get { return _precoVenda; }
+            set { _precoVenda = value; }
+        }
+
+        public string DataValidade
+        {
+            get { return _dataValidade; }
+            set { _dataValidade = value; }
+        }
+
+        public double Peso
+        {
+            get { return _peso; }
+            set { _peso = value; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using lanchonete_OO;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+}
