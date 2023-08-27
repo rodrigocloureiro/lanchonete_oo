e6147950cf2fdf23680a9e58df5034a13b73fe36
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-08-27 17:48:40 -0300

    get, set & metodos Pedido.cs

---
+++ Pedido.cs
@@ -0,0 +1,39 @@
+﻿namespace lanchonete_OO
+{
+    public class Pedido
+    {
+        private string _nomeCliente;
+        private double _taxaServico;
+        private Prato[] _itensConsumidos;
+
+        public string NomeCliente
+        {
+            get { return _nomeCliente; }
+            set { _nomeCliente = value; }
+        }
+
+        public double TaxaServico
+        {
+            get { return _taxaServico; }
+            set { _taxaServico = value; }
+        }
+
+        public Prato[] ItensConsumidos
+        {
+            get { return _itensConsumidos; }
+            set { _itensConsumidos = value; }
+        }
+
+        public void GerarNotaFiscal() { }
+
+        public double CalcularTotal()
+        {
+            return 0;
+        }
+
+        public double CalcularTroco(double valorPago)
+        {
+            return 0;
+        }
+    }
+}
