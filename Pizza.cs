41df8c5efa5d0a7eeb3598ad6fbe28cb2bf8cd78
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-08-30 17:36:49 -0300

    create Pizza.cs

---
+++ Pizza.cs
@@ -0,0 +1,27 @@
+﻿namespace lanchonete_OO
+{
+    public class Pizza : Prato
+    {
+        private string _recheio;
+        private string _borda;
+        private string _molho;
+
+        public string Recheio
+        {
+            get { return _recheio; }
+            set { _recheio = value; }
+        }
+
+        public string Borda
+        {
+            get { return _borda; }
+            set { _borda = value; }
+        }
+
+        public string Molho
+        {
+            get { return _molho; }
+            set { _molho = value; }
+        }
+    }
+}
